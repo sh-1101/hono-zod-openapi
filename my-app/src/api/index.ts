86be2b19bf1f1232d65f233150945892070b36ac
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2024-10-26 23:50:38 +0900

    chore: /api/docへのBasic認証

--- my-app/src/api/index.ts
+++ my-app/src/api/index.ts
@@ -1,6 +1,7 @@
 import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { tasksApi } from "./tasks";
+import { basicAuth } from "hono/basic-auth";
 
 export const api = new OpenAPIHono();
 
@@ -12,6 +13,13 @@ api
       title: "API",
       version: "1.0.0",
     },
+  })
+  .use("/doc/*", async (c, next) => {
+    const auth = basicAuth({
+      username: "user", // 本来は環境変数等でちゃんと値を設定
+      password: "pass", // 今回は固定
+    });
+    return auth(c, next);
   })
   .get(
     "/doc",
